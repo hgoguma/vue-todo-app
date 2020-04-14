e1e08c9fd06c10050386036cfa8027468f156211
hyeleelee <hgoguma124@gmail.com>
2020-04-14 09:34:21 +0900

    todo list 셋팅중

---
+++ vue-todo/src/components/List.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        투두 리스트
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ vue-todo/src/components/ListAdd.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        투두 리스트 추가
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
